5874e8e4364f9f6555342e764cc2c652bfe01a0c
Karim Lmchi <karim.planb@gmail.com>
2021-05-27 15:10:44 +0200

    Add style for TextInput component

--- Components/Search.js
+++ Components/Search.js
@@ -3,9 +3,21 @@ import { View, Button, TextInput } from 'react-native';
 
 export default function Search() {
         return(
-            <View>
-                <TextInput placeholder="Film title"/>
-                <Button title="Search" onPress={() => {}}/>
+            <View style={{ marginTop: 20 }}>
+                <TextInput placeholder="Film title"
+                            style={ styles.textinput }/>
+                <Button title="Search" onPress={() => {}}
+                        style={{ height: 50 }}/>
             </View>
         )
+}
+
+const styles = {
+    textinput: {
+        marginLeft: 5, 
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5,
+    }
 }
