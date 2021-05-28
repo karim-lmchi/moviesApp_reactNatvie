1a32135cfab4767ffeace31ce0e3dac5652407b8
Karim Lmchi <karim.planb@gmail.com>
2021-05-28 10:51:51 +0200

    Add Api Token from the movie data base

---
+++ API/theMovieDataBaseApi.js
@@ -0,0 +1,10 @@
+import API_token from "../Helpers/token";
+
+const API_TOKEN = '5d3ea7738a292dc76fd52915c6b54cdd';
+
+export function getFilmsFromApiWithSearchedText(text) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text;
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => console.log(error))
+}
